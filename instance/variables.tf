4df4828b399567f9b7550995df0d5204a33752d8
Miguel Ignacio Ramos Rebolledo <miguelramos@itesm.mx>
2019-10-30 11:41:13 -0600

    commit inicial, creación de módulos

---
+++ instance/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    default=""
+    description="ID del ami que se va a usar"
+}
+
+variable "instance_type" {
+  
+}
+
+variable "tags" {
+
+}
+
+variable "ingress_rules"{
+
+}
+
+variable "sg_name"{
+    
+}
